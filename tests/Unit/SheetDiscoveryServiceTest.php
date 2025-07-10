848b832ac9039c24c42f8dde5ef5b63568992ff5
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-07-10 17:40:28 +0330

    Avoid base path

--- tests/Unit/SheetDiscoveryServiceTest.php
+++ tests/Unit/SheetDiscoveryServiceTest.php
@@ -3,10 +3,19 @@
 use Akbarjimi\ExcelImporter\Services\SheetDiscoveryService;
 use Akbarjimi\ExcelImporter\Models\ExcelFile;
 
+beforeEach(function () {
+    Storage::fake('local');
+
+    $binary = file_get_contents(__DIR__.'/../stubs/sample.xlsx');
+    Storage::put('imports/sample.xlsx', $binary);
+});
+
 it('discovers sheets from Excel file', function () {
+    $realPath = Storage::disk('local')->path('imports/sample.xlsx');
+
     $file = ExcelFile::create([
         'file_name' => 'sample.xlsx',
-        'path' => base_path('tests/stubs/sample.xlsx'),
+        'path' => $realPath,
         'driver' => 'local',
     ]);
 
