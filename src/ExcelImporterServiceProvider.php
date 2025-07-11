746e81b0cc60c71dc032dcf5a171754e571ddd35
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-07-11 17:02:49 +0330

    Register missing service into CI

--- src/ExcelImporterServiceProvider.php
+++ src/ExcelImporterServiceProvider.php
@@ -3,9 +3,13 @@
 namespace Akbarjimi\ExcelImporter;
 
 use Akbarjimi\ExcelImporter\Events\ExcelUploaded;
+use Akbarjimi\ExcelImporter\Events\FileExtractionCompleted;
 use Akbarjimi\ExcelImporter\Events\SheetsDiscovered;
 use Akbarjimi\ExcelImporter\Listeners\HandleExcelUploaded;
+use Akbarjimi\ExcelImporter\Listeners\HandleFileExtractionCompleted;
 use Akbarjimi\ExcelImporter\Listeners\HandleSheetsDiscovered;
+use Akbarjimi\ExcelImporter\Services\ChunkService;
+use Akbarjimi\ExcelImporter\Services\RowExtractionService;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,8 +20,15 @@ class ExcelImporterServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         Event::listen(ExcelUploaded::class, HandleExcelUploaded::class);
         Event::listen(SheetsDiscovered::class, HandleSheetsDiscovered::class);
+        Event::listen(FileExtractionCompleted::class, HandleFileExtractionCompleted::class);
         $this->publishes([
-            __DIR__.'/config/excel-importer.php' => config_path('excel-importer.php'),
+            __DIR__ . '/config/excel-importer.php' => config_path('excel-importer.php'),
         ], 'config');
+    }
+
+    public function register()
+    {
+        $this->app->bind(ChunkService::class);
+        $this->app->bind(RowExtractionService::class);
     }
 }
