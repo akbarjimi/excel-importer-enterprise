862fabc5a6126bab8faf7a71e6fa6d6452c0f75d
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-07-14 17:55:48 +0330

    Groups row IDs into chunks

--- src/Services/ChunkService.php
+++ src/Services/ChunkService.php
@@ -5,10 +5,11 @@ namespace Akbarjimi\ExcelImporter\Services;
 use Akbarjimi\ExcelImporter\Events\ProcessRowsChunk;
 use Akbarjimi\ExcelImporter\Models\ExcelSheet;
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\Facades\Log;
 
 class ChunkService
 {
-    public function __construct(private Dispatcher $events)
+    public function __construct(private readonly Dispatcher $events)
     {
     }
 
@@ -17,10 +18,22 @@ class ChunkService
         $chunkSize = config('excel-importer.chunk_size', 1000);
 
         $totalRows = $sheet->rows()->count();
-        $chunks = (int)ceil($totalRows / $chunkSize);
-
-        for ($i = 0; $i < $chunks; $i++) {
-            $this->events->dispatch(new ProcessRowsChunk(sheetId: $sheet->id, chunkIndex: $i));
+        if ($totalRows === 0) {
+            Log::info("Sheet [{$sheet->id}] has no rows to process.");
+            return;
         }
+
+        $chunkCount = (int)ceil($totalRows / $chunkSize);
+
+        for ($i = 0; $i < $chunkCount; $i++) {
+            $this->events->dispatch(
+                new ProcessRowsChunk(
+                    sheetId: $sheet->id,
+                    chunkIndex: $i,
+                )
+            );
+        }
+
+        Log::info("Dispatched {$chunkCount} chunk jobs for sheet [{$sheet->id}].");
     }
 }
