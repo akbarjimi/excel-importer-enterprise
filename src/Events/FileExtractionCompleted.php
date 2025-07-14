0821da464266ada9e67955f49d7c133673d5b1df
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-07-14 18:23:06 +0330

    All sheets/rows inserted

--- src/Events/FileExtractionCompleted.php
+++ src/Events/FileExtractionCompleted.php
@@ -4,9 +4,22 @@ namespace Akbarjimi\ExcelImporter\Events;
 
 use Akbarjimi\ExcelImporter\Models\ExcelFile;
 
-class FileExtractionCompleted
+readonly class FileExtractionCompleted
 {
-    public function __construct(public ExcelFile $file)
+    public function __construct(
+        public int $fileId,
+        public int $totalSheets,
+        public int $totalRows,
+    )
     {
+    }
+
+    public static function fromFile(ExcelFile $file): self
+    {
+        return new self(
+            fileId: $file->id,
+            totalSheets: $file->sheets()->count(),
+            totalRows: $file->sheets()->withCount('rows')->get()->sum('rows_count')
+        );
     }
 }
