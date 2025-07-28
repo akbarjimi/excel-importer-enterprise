6aa79af2758b8922a88e4b1508b0c82bbd0f398f
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-07-28 20:05:16 +0330

    Delete dispatcher

--- src/Listeners/HandleExcelUploaded.php
+++ src/Listeners/HandleExcelUploaded.php
@@ -6,14 +6,12 @@ use Akbarjimi\ExcelImporter\Events\ExcelUploaded;
 use Akbarjimi\ExcelImporter\Events\SheetsDiscovered;
 use Akbarjimi\ExcelImporter\Repositories\ExcelSheetRepository;
 use Akbarjimi\ExcelImporter\Services\SheetDiscoveryService;
-use Illuminate\Contracts\Events\Dispatcher;
 
 final readonly class HandleExcelUploaded
 {
     public function __construct(
         private SheetDiscoveryService $discovery,
         private ExcelSheetRepository  $sheetRepo,
-        private Dispatcher            $events,
     )
     {
     }
