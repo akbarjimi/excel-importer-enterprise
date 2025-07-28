0cff9d2c14204cae31b5c04bcc7c43b09f48bd97
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-07-28 17:41:26 +0330

    Replace correct import

--- src/Listeners/HandleSheetsDiscovered.php
+++ src/Listeners/HandleSheetsDiscovered.php
@@ -4,17 +4,21 @@ namespace Akbarjimi\ExcelImporter\Listeners;
 
 use Akbarjimi\ExcelImporter\Events\AllSheetsDispatched;
 use Akbarjimi\ExcelImporter\Events\SheetDiscovered;
+use Akbarjimi\ExcelImporter\Events\SheetsDiscovered;
 use Akbarjimi\ExcelImporter\Repositories\ExcelSheetRepository;
+use Akbarjimi\ExcelImporter\Services\SheetDiscoveryService;
 
 final readonly class HandleSheetsDiscovered
 {
     public function __construct(
-        private ExcelSheetRepository $sheetRepo,
+        private ExcelSheetRepository  $sheetRepo,
+        private SheetDiscoveryService $discovery,
+        private Dispatcher            $events,
     )
     {
     }
 
-    public function handle(SheetDiscovered $event): void
+    public function handle(SheetsDiscovered $event): void
     {
         $sheets = $this->sheetRepo->getByFileId($event->fileId);
         $lastIndex = $sheets->count() - 1;
@@ -23,6 +27,6 @@ final readonly class HandleSheetsDiscovered
             event(new SheetDiscovered($sheet, $i === $lastIndex));
         }
 
-        event(new AllSheetsDispatched($sheets->first()->file));
+        event(new AllSheetsDispatched($event->fileId));
     }
 }
