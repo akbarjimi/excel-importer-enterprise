41475b741d591afb6f18cc246aad98e5555913c2
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-07-28 17:38:50 +0330

    Use file id instead of actual model

--- src/Events/AllSheetsDispatched.php
+++ src/Events/AllSheetsDispatched.php
@@ -2,12 +2,10 @@
 
 namespace Akbarjimi\ExcelImporter\Events;
 
-use Akbarjimi\ExcelImporter\Models\ExcelFile;
-
 readonly class AllSheetsDispatched
 {
     public function __construct(
-        public ExcelFile $file,
+        public int $fileId,
     )
     {
     }
