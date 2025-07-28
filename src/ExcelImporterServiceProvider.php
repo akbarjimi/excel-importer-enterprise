5f1ab4109a6d7c367bb8c933504202648ab28818
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-07-28 17:41:52 +0330

    Moved events to separate method for better readability

--- src/ExcelImporterServiceProvider.php
+++ src/ExcelImporterServiceProvider.php
@@ -4,7 +4,7 @@ namespace Akbarjimi\ExcelImporter;
 
 use Akbarjimi\ExcelImporter\Events\ExcelUploaded;
 use Akbarjimi\ExcelImporter\Events\FileExtractionCompleted;
-use Akbarjimi\ExcelImporter\Events\SheetDiscovered;
+use Akbarjimi\ExcelImporter\Events\SheetsDiscovered;
 use Akbarjimi\ExcelImporter\Listeners\HandleExcelUploaded;
 use Akbarjimi\ExcelImporter\Listeners\HandleFileExtractionCompleted;
 use Akbarjimi\ExcelImporter\Listeners\HandleSheetsDiscovered;
@@ -17,12 +17,10 @@ class ExcelImporterServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        Event::listen(ExcelUploaded::class, HandleExcelUploaded::class);
-        Event::listen(SheetDiscovered::class, HandleSheetsDiscovered::class);
-        Event::listen(FileExtractionCompleted::class, HandleFileExtractionCompleted::class);
+        $this->registerEventListeners();
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         $this->publishes([
-            __DIR__.'/config/excel-importer.php' => config_path('excel-importer.php'),
+            __DIR__ . '/config/excel-importer.php' => config_path('excel-importer.php'),
         ], 'config');
     }
 
@@ -31,9 +29,15 @@ class ExcelImporterServiceProvider extends ServiceProvider
         $this->app->bind(ChunkService::class);
         $this->app->bind(RowExtractionService::class);
 
-        // This is the crucial line for package config loading
         $this->mergeConfigFrom(
-            __DIR__.'/config/excel-importer.php', 'excel-importer'
+            __DIR__ . '/config/excel-importer.php', 'excel-importer'
         );
+    }
+
+    public function registerEventListeners(): void
+    {
+        Event::listen(ExcelUploaded::class, HandleExcelUploaded::class);
+        Event::listen(SheetsDiscovered::class, HandleSheetsDiscovered::class);
+        Event::listen(FileExtractionCompleted::class, HandleFileExtractionCompleted::class);
     }
 }
