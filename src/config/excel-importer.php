2e33ce66d0f22e39922eed4803f9cb32fdde150d
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-08-02 17:34:51 +0330

    Added hash_algo to config to ensure do not duplicate content if hash algo changed along the way

--- src/config/excel-importer.php
+++ src/config/excel-importer.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-
     'chunk_size' => env('EXCEL_IMPORTER_CHUNK_SIZE', 1000),
-
     'insert_batch_size' => env('EXCEL_IMPORTER_INSERT_BATCH_SIZE', 100),
-
     'default_disk' => env('EXCEL_IMPORTER_DISK', 'local'),
+    'hash_algo' => env('EXCEL_IMPORTER_HASH_ALGO', 'md5'),
 ];
