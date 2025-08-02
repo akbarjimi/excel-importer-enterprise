e95e2cdeda32344932a2a80f1714dec63c9f1cf2
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-08-02 15:22:44 +0330

    Delete queuable from listener

--- src/Listeners/HandleSheetDiscovered.php
+++ src/Listeners/HandleSheetDiscovered.php
@@ -4,13 +4,14 @@ namespace Akbarjimi\ExcelImporter\Listeners;
 
 use Akbarjimi\ExcelImporter\Events\SheetDiscovered;
 use Akbarjimi\ExcelImporter\Services\RowExtractionService;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
-final readonly class HandleSheetDiscovered implements ShouldQueue
+final readonly class HandleSheetDiscovered
 {
     public function __construct(
         private RowExtractionService $extractor,
-    ) {}
+    )
+    {
+    }
 
     public function handle(SheetDiscovered $event): void
     {
