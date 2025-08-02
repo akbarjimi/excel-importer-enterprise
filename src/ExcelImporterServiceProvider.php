e1f07e0e1819733a51676a1dcd33c99d09ea513d
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-08-02 15:23:52 +0330

    Register missing event/listeners

--- src/ExcelImporterServiceProvider.php
+++ src/ExcelImporterServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace Akbarjimi\ExcelImporter;
 
+use Akbarjimi\ExcelImporter\Events\AllSheetsDispatched;
 use Akbarjimi\ExcelImporter\Events\ExcelUploaded;
-use Akbarjimi\ExcelImporter\Events\FileExtractionCompleted;
+use Akbarjimi\ExcelImporter\Events\SheetDiscovered;
 use Akbarjimi\ExcelImporter\Events\SheetsDiscovered;
+use Akbarjimi\ExcelImporter\Listeners\HandleAllSheetsDispatched;
 use Akbarjimi\ExcelImporter\Listeners\HandleExcelUploaded;
-use Akbarjimi\ExcelImporter\Listeners\HandleFileExtractionCompleted;
+use Akbarjimi\ExcelImporter\Listeners\HandleSheetDiscovered;
 use Akbarjimi\ExcelImporter\Listeners\HandleSheetsDiscovered;
 use Akbarjimi\ExcelImporter\Services\ChunkService;
 use Akbarjimi\ExcelImporter\Services\RowExtractionService;
@@ -38,6 +40,7 @@ class ExcelImporterServiceProvider extends ServiceProvider
     {
         Event::listen(ExcelUploaded::class, HandleExcelUploaded::class);
         Event::listen(SheetsDiscovered::class, HandleSheetsDiscovered::class);
-        Event::listen(FileExtractionCompleted::class, HandleFileExtractionCompleted::class);
+        Event::listen(SheetDiscovered::class, HandleSheetDiscovered::class);
+        Event::listen(AllSheetsDispatched::class, HandleAllSheetsDispatched::class);
     }
 }
