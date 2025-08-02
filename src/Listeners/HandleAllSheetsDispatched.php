b10784d938d0b8c4c09896b174efa4452bd5fe53
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-08-02 15:22:18 +0330

    Added missing listener

---
+++ src/Listeners/HandleAllSheetsDispatched.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Akbarjimi\ExcelImporter\Listeners;
+
+use Akbarjimi\ExcelImporter\Events\SheetDiscovered;
+
+final readonly class HandleAllSheetsDispatched
+{
+    public function __construct()
+    {
+    }
+
+    public function handle(SheetDiscovered $event): void
+    {
+        // do nothing for now
+    }
+}
