89049f2bdf1d03489b4e2060f1520ad1e55ec668
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-08-31 16:38:09 +0330

    Use test config and separate transformer and validation tests

--- tests/Feature/Chunking/TransformerValidatorTest.php
+++ tests/Feature/Chunking/TransformerValidatorTest.php
@@ -4,30 +4,35 @@ use Akbarjimi\ExcelImporter\Models\ExcelSheet;
 use Akbarjimi\ExcelImporter\Services\TransformService;
 use Akbarjimi\ExcelImporter\Services\ValidateService;
 
-it('applies transformer and validation correctly', function () {
-    // Arrange
-    $sheet = new ExcelSheet(['name' => 'Sheet1']);
-    $rowContent = ['A1' => 'hello', 'B1' => ' 123 '];
+beforeEach(function () {
+    Config::set('excel-importer-sheets', require_once __DIR__ . "/../../_fixtures/config/excel-importer-sheets.php");
+});
 
-    $transform = new TransformService();
-    $validate = new ValidateService();
+it('applies transformer correctly', function () {
+    $sheet = ExcelSheet::factory()->make(['name' => 'Sheet1']);
+    $rowContent = ['A1' => 'hello', 'B1' => '123'];
 
+    $transform = app(TransformService::class);
     $transform->load($sheet);
-    $validate->load($sheet);
 
-    // Act
     $transformed = $transform->apply($rowContent);
-    $errors = $validate->apply($transformed);
 
-    // Assert
     expect($transformed['A1'])->toBe('HELLO');
     expect($transformed['B1'])->toBe('123');
-    expect($errors)->toBeEmpty();
+});
 
-    // Introduce validation error
+it('applies validator correctly', function () {
+    $sheet = ExcelSheet::factory()->make(['name' => 'Sheet1']);
+    $validRow = ['A1' => 'HELLO', 'B1' => '123'];
     $invalidRow = ['A1' => '', 'B1' => 'abc'];
-    $errors = $validate->apply($invalidRow);
 
-    expect($errors)->not()->toBeEmpty();
-    expect($errors)->toHaveKeys(['A1', 'B1']);
+    $validate = app(ValidateService::class);
+    $validate->load($sheet);
+
+    $errorsValid = $validate->apply($validRow);
+    $errorsInvalid = $validate->apply($invalidRow);
+
+    expect($errorsValid)->toBeEmpty();
+    expect($errorsInvalid)->not()->toBeEmpty();
+    expect($errorsInvalid)->toHaveKeys(['A1', 'B1']);
 });
