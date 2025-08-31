849c7916b0b72b6339203276eb8ddd1a5c89fde7
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-08-31 17:24:06 +0330

    Changed a testing validation

--- tests/_fixtures/config/excel-importer-sheets.php
+++ tests/_fixtures/config/excel-importer-sheets.php
@@ -11,7 +11,7 @@ return [
         'validation' => [
             'A1' => 'required|string|max:255',
             'B1' => 'required|email',
-            'C1' => 'required|int',
+            'C1' => 'required|min:31',
         ],
     ],
     // 2sheets2rows.xlsx
