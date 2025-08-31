6e457eaf7fa8bcbe4b53087dfafdc62330c290f1
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-08-31 17:23:47 +0330

    Empty developer excel-importer-sheets.php

--- src/config/excel-importer-sheets.php
+++ src/config/excel-importer-sheets.php
@@ -1,55 +1,5 @@
 <?php
 
 return [
-    // 1sheet3rows1header.xlsx
-    'Sheet1' => [
-        'transformers' => [
-            'A1' => fn($value) => strtoupper($value),
-            'B1' => fn($value) => strtoupper($value),
-            'C1' => fn($value) => strtoupper($value),
-        ],
-        'validation' => [
-            'A1' => 'required|string|max:255',
-            'B1' => 'required|email',
-            'C1' => 'required|int',
-        ],
-    ],
-    // 2sheets2rows.xlsx
-    'Sheet2' => [
-        'transformers' => [
-            'A1' => fn($value) => intval($value),
-            'B1' => fn($value) => intval($value),
-        ],
-        'validation' => [
-            'A1' => 'required|int',
-            'B1' => 'required|int',
-        ],
-    ],
-    'Sheet3' => [
-        'transformers' => [
-            'A1' => fn($value) => intval($value),
-            'B1' => fn($value) => intval($value),
-        ],
-        'validation' => [
-            'A1' => 'required|int',
-            'B1' => 'required|int',
-        ],
-    ],
-    // 2sheets2000rows.xlsx
-    'Sheet4' => [
-        'transformers' => [
-            'A1' => fn($value) => intval($value),
-        ],
-        'validation' => [
-            'A1' => 'required|int|max:2000',
-        ],
-    ],
-    'Sheet5' => [
-        'transformers' => [
-            'A1' => fn($value) => intval($value),
-        ],
-        'validation' => [
-            'A1' => 'required|int|max:2000',
-        ],
-    ],
+
 ];
