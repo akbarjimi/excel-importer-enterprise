3ea07bf86ff9fc5e677452e7fec56a9fb079b8d2
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-09-05 13:07:21 +0330

    Delete unnecessary Repository lines ans classes

--- src/Repositories/ExcelRowRepository.php
+++ src/Repositories/ExcelRowRepository.php
@@ -6,11 +6,6 @@ use Akbarjimi\ExcelImporter\Models\ExcelRow;
 
 class ExcelRowRepository
 {
-    public function bulkInsert(array $rows): void
-    {
-        ExcelRow::insert($rows);
-    }
-
     public function bulkUpsert(array $rows): void
     {
         ExcelRow::upsert(
@@ -18,12 +13,5 @@ class ExcelRowRepository
             ['excel_sheet_id', 'content_hash', 'hash_algo'],
             ['content', 'status', 'chunk_index', 'row_index', 'updated_at']
         );
-    }
-
-    public function updateChunkIndex(int $sheetId, int $rowIndex, int $chunkIndex): void
-    {
-        ExcelRow::where('excel_sheet_id', $sheetId)
-            ->where('row_index', $rowIndex)
-            ->update(['chunk_index' => $chunkIndex]);
     }
 }
