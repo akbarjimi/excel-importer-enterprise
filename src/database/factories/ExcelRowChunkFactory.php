b14b1c4e93687d085c7e4be5c4c0beed7bdf4a3a
Mohammad Akbari <mohammad.akbari@outlook.com>
2025-09-22 19:04:13 +0330

    Added missing ExcelRowChunkFactory

---
+++ src/database/factories/ExcelRowChunkFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Akbarjimi\ExcelImporter\Models\ExcelRowChunk;
+use Akbarjimi\ExcelImporter\Models\ExcelSheet;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ExcelRowChunkFactory extends Factory
+{
+    protected $model = ExcelRowChunk::class;
+
+    public function definition(): array
+    {
+        return [
+            'excel_sheet_id' => ExcelSheet::factory(),
+            'from_row_id' => 1,
+            'to_row_id' => 3,
+            'size' => 3,
+            'status' => 'pending',
+            'mapping_status' => 'pending',
+            'attempts' => 0,
+            'error' => null,
+            'dispatched_at' => null,
+            'processed_at' => null,
+        ];
+    }
+}
